f37c91f9420fb49dfc9a69e3773fce25432b9127
SAN_INV <y9_mochi@yahoo.co.jp>
2024-10-26 14:05:32 +0900

    commit yamanaka test 2024/10/26 14:05:32

---
+++ app/dashboard/invoices/error.tsx
@@ -0,0 +1,37 @@
+'use client';
+ 
+import { useEffect } from 'react';
+ 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error
+
+
+// error: This object is an instance of JavaScript's native Error object.
+// reset: This is a function to reset the error boundary. When executed, the function will try to re-render the route segment.
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Optionally log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+ 
+  return (
+    <main className="flex h-full flex-col items-center justify-center">
+      <h2 className="text-center">Something went wrong!</h2>
+      <button
+        className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
+        onClick={
+          // Attempt to recover by trying to re-render the invoices route
+          () => reset()
+        }
+      >
+        Try again
+      </button>
+    </main>
+  );
+}
